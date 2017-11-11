0911e3feb37e7c062041fc279e70a7a4e77da193
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-11-11 13:04:36 -0200

    remove todo

--- tokens.go
+++ tokens.go
@@ -6,7 +6,6 @@ import (
 	"golang.org/x/oauth2"
 )
 
-// TODO: break this in smaller interfaces and compose
 type TokensSvc interface {
 	Exists(userID int64) (bool, error)
 	Save(userID int64, token *oauth2.Token) error
